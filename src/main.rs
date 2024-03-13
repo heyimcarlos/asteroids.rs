5c85a8410cef714974ebac3caa842f7482e6ce7d
Carlos De la Cruz <me@heyimcarlos.dev>
2024-03-13 00:25:13 -0400

    chore: add initial plugins for spaceship game tutorial

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+mod camera;
+mod debug;
+mod movement;
+mod spaceship;
+
+use bevy::prelude::*;
+use camera::CameraPlugin;
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        // Bevy built-ins.
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
+        // user configured plugins.
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(CameraPlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(DebugPlugin)
+        .run();
+}
